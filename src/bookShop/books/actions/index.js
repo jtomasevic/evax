2bec6cdec89d14abb6c7b55d978b97714fd3771b
Jovan <jtomasevic@gmail.com>
2020-01-04 00:38:14 +0100

    add actions for filtering-searhing books

--- src/bookShop/books/actions/index.js
+++ src/bookShop/books/actions/index.js
@@ -29,7 +29,17 @@ export const booksLoadingError = (error) => ({
 });
 
 /**
- * 'Action creator' for loading list of books. This method is executing asynchorny and dispatch diffrent
+ * Action to be called when user wants to search for books.
+ * @param {string} filter use this value to query books
+ */
+export const filteringBooks = (filter: string) => ({
+    type: 'filteringBooks',
+    status: 'filteringBooks',
+    filter
+});
+
+/**
+ * Action creator for loading list of books. This method is executing asynchorny and dispatch diffrent
  * actions during fetching books like: started, ok, notok.
  * @param {Function} dispatch function passed via Evax library to be used to dispatch actions
  */
@@ -39,5 +49,28 @@ export const loadBooks = (dispatch: Function) => {
         dispatch(booksLoaded(books));
     }).catch(error => {
         dispatch(booksLoadingError(error));
+    });
+};
+
+let rejectFilterFetch = null;
+/**
+ * Action creator for filtering/searching books.
+ * It is using loadBooks action creator for loading books.
+ */
+export const filterBooks = (phrase: string, dispatch: Function) => {
+    dispatch(filteringBooks(phrase));
+    // we want to enable smooth filtering, without breaking while use typing key words.
+    // so until user stop typing or make pause, we don' want to have non-neccessary, interupting rendering
+    if (rejectFilterFetch) {
+        rejectFilterFetch();
+    }
+    const fetchBooks: Promise<Array<Book>> = new Promise<Array<Book>>((resolve, reject) => {
+        rejectFilterFetch = reject;
+        getBooks(phrase).then((books: Array<Book>) => {
+            resolve(books);
+        });
+    });
+    fetchBooks.then(books => {
+        dispatch(booksLoaded(books));
     });
 };
